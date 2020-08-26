5353b00b6a0ab15013422b726466883581a72966
danny-pantoja <danny.pantoja.23@gmail.com>
2020-08-26 13:12:33 -0700

    dp adding Items.js

---
+++ src/components/Items/Items.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+// import Layout from '../shared/Layout'
+
+// import the api's url
+import apiUrl from '../../apiConfig'
+
+// Import axios so we can make HTTP requests
+import axios from 'axios'
+
+// This will be our Items Index component (show all items)
+class Items extends Component {
+  constructor (props) {
+    super(props)
+
+    // setup our initial state
+    this.state = {
+      // we have zero items, until our API request has finished
+      items: []
+    }
+  }
+
+  // this is called whenever our component is created and inserted
+  // into the DOM (first appears)
+  componentDidMount () {
+    // make a GET request for all of the items
+    axios({
+      url: `${apiUrl}/items`,
+      headers: {
+        'Authorization': `Token token=${this.props.user.token}`
+      }
+    })
+      .then(res => this.setState({ items: res.data.items }))
+      .catch(console.error)
+  }
+
+  render () {
+    const items = this.state.items.map(item => (
+      <li key={item._id}>
+        <Link to={`/items/${item._id}`}>
+          {item.title}
+        </Link>
+      </li>
+    ))
+
+    return (
+      <div>
+        <h4>Items</h4>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Items
