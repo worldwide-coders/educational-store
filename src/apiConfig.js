b14a5e3533823c67c16962d34e1e72ec530fb12d
Sebastian <sebastian.chang23@gmail.com>
2020-08-27 09:55:10 -0500

    Added backend url to api config file

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://thawing-basin-32932.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
