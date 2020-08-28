eb755d660c4bbb4dff8192a9dde14737441563d2
danny-pantoja <danny.pantoja.23@gmail.com>
2020-08-28 12:43:26 -0700

    Committing my feature to test

--- src/components/ItemsCards/ItemsCards.js
+++ src/components/ItemsCards/ItemsCards.js
@@ -1,35 +1,28 @@
-import { Card } from 'react-router-card'
+import React from 'react'
+import Card from 'react-bootstrap/Card'
 // <Button variant="primary">Go somewhere</Button>
 
+const ItemsCards = ({ card }) => {
+  return (
+    <React.Fragment className="EducationalStore">
+      <Card style={{ width: '18rem' }} key={card._id}>
+        <Card.Img variant="top" src={card.imageUrl} />
+        <Card.Body>
+          <Card.Title>{card.name}</Card.Title>
+          <Card.Text>
+            {card.price}
+          </Card.Text>
 
-
-
-const ItemsCards = () => {
-  const cardInfo = [
-    { image: '', title: '', text: ''},
-    { image: '', title: '', text: ''},
-    { image: '', title: '', text: ''}
-  ]
-}
-
-const renderCard = (card, index) => {
-  return(
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Name>Card Title</Card.Name>
-      <Card.Price>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
-        </Card.Price>
-
-      </Card.Body>
+        </Card.Body>
       </Card>
+    </React.Fragment>
   )
 }
 
-return (
-
-)
-
 export default ItemsCards
+
+// const cardInfo = [
+//   { image: '', title: '', text: '' },
+//   { image: '', title: '', text: '' },
+//   { image: '', title: '', text: '' }
+// ]
