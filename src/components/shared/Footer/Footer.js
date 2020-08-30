a7570ffad6dc3f17c076a7946d06858b5d5a8c7d
adamrturman <adamrturman@gmail.com>
2020-08-29 19:56:24 -0500

    added link to github in footer component

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (
-  <p>&#169; 2020 worldwidecoders </p>
+  <p><a href="https://github.com/worldwide-coders">&#169; 2020 worldWideCoders</a></p>
 )
 
 export default Footer
