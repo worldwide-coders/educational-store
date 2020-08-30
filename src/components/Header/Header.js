0d0c39ab5fbaed65871db2db71ce2b80ba747d59
adamrturman <adamrturman@gmail.com>
2020-08-29 20:40:18 -0500

    Header Styling
    Added drop downs for similar features
    Made 'An Educational Store' title a link to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import styles from './Header.css'
 
 const authenticatedOptions = (cart) => (
   <Fragment>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <NavDropdown title="Options" alignRight id="settings-dropdown">
+      <NavDropdown.Item href="#change-password">Change Password</NavDropdown.Item>
+      <NavDropdown.Divider />
+      <NavDropdown.Item href="#sign-out">Sign Out</NavDropdown.Item>
+    </NavDropdown>
     <Nav.Link href={`#carts/${cart}`}>My Cart</Nav.Link>
     <Nav.Link href="#carts">All Orders</Nav.Link>
   </Fragment>
@@ -14,8 +18,11 @@ const authenticatedOptions = (cart) => (
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <NavDropdown title="Login" alignRight id="settings-dropdown">
+      <NavDropdown.Item href="#sign-up">Sign Up</NavDropdown.Item>
+      <NavDropdown.Divider />
+      <NavDropdown.Item href="#sign-in">Sign In</NavDropdown.Item>
+    </NavDropdown>
   </Fragment>
 )
 
@@ -28,8 +35,8 @@ const alwaysOptions = (
 )
 
 const Header = ({ user, cart }) => (
-  <Navbar className={ user ? '' : styles.navbarBackground} variant="dark" expand="md">
-    <Navbar.Brand>An Educational Supply Donation Store
+  <Navbar style={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(66,66,128,1) 50%, rgba(46,109,166,1) 83%, rgba(0,212,255,1) 100%)' }} className={ user ? '' : styles.navbarBackground} variant="dark" expand="md">
+    <Navbar.Brand href="#">An Educational Supply Donation Store
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
